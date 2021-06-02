01adb6354e0c4d027cd0bc330eabd2888af7ec5b
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2021-06-02 21:21:05 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ read -sp "boomiEnvironmentId: " boomiEnvironmentId
 mkdir /home/ubuntu/boomi-iot-simulation/boomi_dir
 mkdir /home/ubuntu/boomi-iot-simulation/boomi_dir/test
 chmod 777 /home/ubuntu/boomi-iot-simulation/boomi_dir/test
-cd /home/ubuntu/boomi-iot-simulation
-wget docker-compose.yml
+cd /home/ubuntu/boomi-iot-simulation && wget https://github.com/thecloudgarage/Boomi-based-simulator/blob/main/docker-compose.yml
 sed -i "s/boomiToken/$boomiToken/g" /home/ubuntu/boomi-iot-simulation/docker-compose.yml
 sed -i "s/boomiEnvironmentId/$boomiEnvironmentId/g" /home/ubuntu/boomi-iot-simulation/docker-compose.yml
 mkdir /home/ubuntu/boomi-iot-simulation
 mkdir /home/ubuntu/boomi-iot-simulation/portainer
 mkdir /home/ubuntu/boomi-iot-simulation/mosquitto
-cd /home/ubuntu/boomi-iot-simulation/mosquitto && wget abc.conf
+cd /home/ubuntu/boomi-iot-simulation/mosquitto && wget https://github.com/thecloudgarage/Boomi-based-simulator/blob/main/mosquitto.conf
 cd /home/ubuntu/boomi-iot-simulation && docker-compose up -d
+echo "done"
