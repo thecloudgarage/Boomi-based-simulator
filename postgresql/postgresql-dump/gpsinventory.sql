d0aad1d50969098014ad6e16c932153788cbb5bf
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2021-06-05 18:56:59 +0530

    Update gpsinventory.sql

--- postgresql/postgresql-dump/gpsinventory.sql
+++ postgresql/postgresql-dump/gpsinventory.sql
@@ -7,13 +7,10 @@ CREATE SEQUENCE gpsinventory_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 922337203685
 CREATE TABLE "public"."gpsinventory" (
     "id" integer DEFAULT nextval('gpsinventory_id_seq') NOT NULL,
     "gpsterminalid" text,
-    "country" text,
-    "state" text,
-    "city" text,
     "customerId" text,
     "customerName" text,
-    "latitude" double precision,
-    "longitude" double precision,
+    "lat" double precision,
+    "long" double precision,
     "timestamp" timestamptz,
     CONSTRAINT "gpsinventory_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
@@ -22,12 +19,12 @@ CREATE INDEX "gpsinventory_customerId" ON "public"."gpsinventory" USING btree ("
 
 CREATE INDEX "gpsinventory_gpsterminalid" ON "public"."gpsinventory" USING btree ("gpsterminalid");
 
-INSERT INTO "gpsinventory" ("id", "gpsterminalid", "country", "state", "city", "customerId", "customerName", "latitude", "longitude", "timestamp") VALUES
-(1,     'gps-001-0001', 'india',        'delhi',        'delhi',        '1',    'abc001 private limited',       NULL,   NULL,   NULL),
-(2,     'gps-002-0001', 'india',        'maharashtra',  'mumbai',       '2',    'abc002 private limited',       NULL,   NULL,   NULL),
-(3,     'gps-003-0001', 'india',        'karnataka',    'bangalore',    '3',    'abc003 private limited',       NULL,   NULL,   NULL),
-(4,     'gps-004-0001', 'india',        'tamilnadu',    'chennai',      '4',    'abc004 private limited',       NULL,   NULL,   NULL),
-(5,     'gps-005-0001', 'india',        'westbengal',   'kolkatta',     '5',    'abc005 private limited',       NULL,   NULL,   NULL),
-(6,     'gps-006-0001', 'india',        'rajasthan',    'jaipur',       '6',    'abc006 private limited',       NULL,   NULL,   NULL);
+INSERT INTO "gpsinventory" ("id", "gpsterminalid", "customerId", "customerName", "lat", "long", "timestamp") VALUES
+(1,	'gps-001-0001',	'1',	'abc001 private limited',	NULL,	NULL,	NULL),
+(2,	'gps-002-0001',	'2',	'abc002 private limited',	NULL,	NULL,	NULL),
+(3,	'gps-003-0001',	'3',	'abc003 private limited',	NULL,	NULL,	NULL),
+(4,	'gps-004-0001',	'4',	'abc004 private limited',	NULL,	NULL,	NULL),
+(5,	'gps-005-0001',	'5',	'abc005 private limited',	NULL,	NULL,	NULL),
+(6,	'gps-006-0001',	'6',	'abc006 private limited',	NULL,	NULL,	NULL);
 
--- 2021-06-05 06:48:59.065664+00
+-- 2021-06-05 12:55:55.061217+00
